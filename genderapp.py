31a7b14fcd166ed26836d86a7ec0a6c31eb82895
Jason Carlisle Mann <m5praetor@gmail.com>
2013-03-06 15:58:18 -0500

    init_db added

--- genderapp.py
+++ genderapp.py
@@ -2,6 +2,8 @@
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
 	 abort, render_template, flash
+from __future__ import with_statement
+from contextlib import closing
 
 # configuration
 DATABASE = '/tmp/genderapp.db'
@@ -16,8 +18,23 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
+
+
+
+###############################################################################
+# DATABASE FUNCTIONS
+
 def connect_db():
 	return sqlite3.connect(app.config['DATABASE'])
+
+def init_db():
+	with closing(connect_db()) as db:
+		with app.open_resource('schema.sql') as f:
+			db.cursor().executescript(f.read())
+		db.commit()
+###############################################################################
+
+
 
 
 if __name__ == '__main__':
