c167d5773203ba79b517b1adeccac9b725ac2099
Jason Carlisle Mann <m5praetor@gmail.com>
2013-03-06 20:26:10 -0500

    fixed db stuff, played with tables, added some templates

--- gendertest.py
+++ gendertest.py
@@ -32,21 +32,88 @@ def init_db():
 
 @app.before_request
 def before_request():
-    g.db = connect_db()
+	g.db = connect_db()
 
 @app.teardown_request
 def teardown_request(exception):
-    g.db.close()
+	g.db.close()
+
+def query_db(query, args=(), one=False):
+	cur = g.db.execute(query, args)
+	rv = [dict((cur.description[idx][0], value)
+			for idx, value in enumerate(row))
+			 for row in cur.fetchall()]
+
+	return (rv[0] if rv else None) if one else rv
 ###############################################################################
 
 
 ###############################################################################
 # APP CODE
+@app.route('/')
+def show_participants():
+	cur = g.db.execute('select  \
+			age, gender, nat_lang, oth_lang, years_eng, years_lived \
+			from entries order by p_id desc')
+	entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+	return render_template('show_participants.html', entries=entries)
+
+@app.route('/add_participant', methods=['POST'])
+def add_participant():
+	if not session.get('logged_in'):
+		abort(401)
+	g.db.execute('insert into entries \
+			(age, gender, nat_lang, oth_lang, years_eng, years_lived) \
+			values (?, ?)', [request.form['age'], request.form['gender'], \
+			request.form['nat_lang'], request.form['oth_lang'] \
+			request.form['years_eng'], request.form['years_lived']])
+	g.db.commit()
+
+	flash('New entry was successfully posted')
+	return redirect(url_for('show_participants'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_participants'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_participants'))
+
+###############################################################################
 
 
+
+
+
+
+
+
+
+###############################################################################
+## RUN THE PROGRAM
 if __name__ == '__main__':
 	# must get the port number from heroku
 	# environment variable and look for all open
 	# connections
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=port)
+
+
+
+
+
+
+
--- schema.sql
+++ schema.sql
@@ -1,7 +1,18 @@
-drop table if exists entries;
+drop table if exists participants;
 
-create table entries (
-  id integer primary key autoincrement,
-  title string not null,
-  text string not null
+create table participants (
+  p_id integer primary key autoincrement,
+  age string not null,
+  gender string not null,
+  nat_lang string not null,
+  oth_lang string not null,
+  years_eng integer not null,
+  years_lived integer not null
+);
+
+drop table if exists answers;
+
+create table answers (
+  p_id integer primary key autoincrement,
+  pcent_delimited_ans string
 );
